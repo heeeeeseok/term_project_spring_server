933d665a723a64ca6071c177ac1c147cb507816c
seokhee <skdlzl753@naver.com>
2023-07-27 13:48:17 +0900

    debug post save

--- src/main/java/com/example/term_project/main/global/service/S3UploadService.java
+++ src/main/java/com/example/term_project/main/global/service/S3UploadService.java
@@ -3,6 +3,8 @@ package com.example.term_project.main.global.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,18 +16,28 @@ import java.io.IOException;
 public class S3UploadService {
 
     private final AmazonS3 amazonS3;
+    private final Logger LOGGER = LoggerFactory.getLogger(S3UploadService.class);
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
     public String saveFile(MultipartFile multipartFile) throws IOException {
+        LOGGER.info("debug1");
         String originalFilename = multipartFile.getOriginalFilename();
+        LOGGER.info("debug2");
 
         ObjectMetadata metadata = new ObjectMetadata();
+        LOGGER.info("debug3");
+
         metadata.setContentLength(multipartFile.getSize());
+        LOGGER.info("debug4");
+
         metadata.setContentType(multipartFile.getContentType());
+        LOGGER.info("debug5");
 
         amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
+        LOGGER.info("debug6");
+
         return amazonS3.getUrl(bucket, originalFilename).toString();
     }
 
